42994f3b41d8189da3a7d5e9e2dafb768c192658
Elliot Hogg <elliothogg@live.com>
2021-08-07 01:02:11 +0100

    rewritten c-gan model. Removed redundant files

---
+++ chord_generator/c_gan_model.py
@@ -0,0 +1,232 @@
+import pickle
+from numpy import expand_dims
+from numpy import zeros
+from numpy import ones
+from numpy.random import randn
+from numpy.random import randint
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Reshape
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.layers import Concatenate
+import numpy as np
+from matplotlib import pyplot
+
+
+def load_data():
+    data_pickle_path = 'chord_matrices_training_data.pickle'
+    with open(data_pickle_path, 'rb') as file:
+        training_data = pickle.load(file)
+    t_d = reshape_12_x_12(training_data)
+    return t_d
+
+def reshape_12_x_12(data):
+    y, x = data
+    x_reshaped = []
+    for voicing in x:
+        reshape = voicing.copy()
+        reshape.resize(12,12)
+        x_reshaped.append(reshape)
+    x_reshaped = np.array(x_reshaped)
+    return (y, x_reshaped)
+
+# define the standalone discriminator model
+def define_discriminator(in_shape=(12,12,1), n_classes=3):
+    # label input
+    in_label = Input(shape=(1,))
+    # embedding for categorical input
+    li = Embedding(n_classes, 50)(in_label)
+    # scale up to image dimensions with linear activation
+    n_nodes = in_shape[0] * in_shape[1]
+    li = Dense(n_nodes)(li)
+    # reshape to additional channel
+    li = Reshape((in_shape[0], in_shape[1], 1))(li)
+    # image input
+    in_image = Input(shape=in_shape)
+    # concat label as a channel
+    merge = Concatenate()([in_image, li])
+    # downsample
+    fe = Conv2D(128, (3,3), strides=(2,2), padding='same')(merge)
+    fe = LeakyReLU(alpha=0.2)(fe)
+    # downsample
+    fe = Conv2D(128, (3,3), strides=(2,2), padding='same')(fe)
+    fe = LeakyReLU(alpha=0.2)(fe)
+    # flatten feature maps
+    fe = Flatten()(fe)
+    # dropout
+    fe = Dropout(0.4)(fe)
+    # output
+    out_layer = Dense(1, activation='sigmoid')(fe)
+    # define model
+    model = Model([in_image, in_label], out_layer)
+    # compile model
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+    return model
+
+# define the standalone generator model
+def define_generator(latent_dim, n_classes=3):
+    # label input
+    in_label = Input(shape=(1,))
+    # embedding for categorical input
+    li = Embedding(n_classes, 50)(in_label)
+    # linear multiplication
+    n_nodes = 3 * 3
+    li = Dense(n_nodes)(li)
+    # reshape to additional channel
+    li = Reshape((3, 3, 1))(li)
+    # image generator input
+    in_lat = Input(shape=(latent_dim,))
+    # foundation for 3x3 image
+    n_nodes = 128 * 3 * 3
+    gen = Dense(n_nodes)(in_lat)
+    gen = LeakyReLU(alpha=0.2)(gen)
+    gen = Reshape((3, 3, 128))(gen)
+    # merge image gen and label input
+    merge = Concatenate()([gen, li])
+    # upsample to 6x6
+    gen = Conv2DTranspose(128, (4,4), strides=(2,2), padding='same')(merge)
+    gen = LeakyReLU(alpha=0.2)(gen)
+    # upsample to 12x12
+    gen = Conv2DTranspose(128, (4,4), strides=(2,2), padding='same')(gen)
+    gen = LeakyReLU(alpha=0.2)(gen)
+    # output
+    out_layer = Conv2D(1, (7,7), activation='tanh', padding='same')(gen)
+    # define model
+    model = Model([in_lat, in_label], out_layer)
+    return model
+
+# define the combined generator and discriminator model, for updating the generator
+def define_gan(g_model, d_model):
+    # make weights in the discriminator not trainable
+    d_model.trainable = False
+    # get noise and label inputs from generator model
+    gen_noise, gen_label = g_model.input
+    # get image output from the generator model
+    gen_output = g_model.output
+    # connect image output and label input from generator as inputs to discriminator
+    gan_output = d_model([gen_output, gen_label])
+    # define gan model as taking noise and label and outputting a classification
+    model = Model([gen_noise, gen_label], gan_output)
+    # compile model
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss='binary_crossentropy', optimizer=opt)
+    return model
+
+# load fashion mnist images
+def load_real_samples():
+    # load dataset
+    trainy, trainX = load_data()
+    # expand to 3d, e.g. add channels
+    X = expand_dims(trainX, axis=-1)
+
+    return [X, trainy]
+
+# # select real samples
+def generate_real_samples(dataset, n_samples):
+    # split into images and labels
+    images, labels = dataset
+    # choose random instances
+    ix = randint(0, images.shape[0], n_samples)
+    # select images and labels
+    X, labels = images[ix], labels[ix]
+    # generate class labels
+    y = ones((n_samples, 1))
+    return [X, labels], y
+
+# generate points in latent space as input for the generator
+def generate_latent_points(latent_dim, n_samples, n_classes=3):
+    # generate points in the latent space
+    x_input = randn(latent_dim * n_samples)
+    # reshape into a batch of inputs for the network
+    z_input = x_input.reshape(n_samples, latent_dim)
+    # generate labels
+    labels = randint(0, n_classes, n_samples)
+    return [z_input, labels]
+
+# use the generator to generate n fake examples, with class labels
+def generate_fake_samples(generator, latent_dim, n_samples):
+    # generate points in latent space
+    z_input, labels_input = generate_latent_points(latent_dim, n_samples)
+    # predict outputs
+    images = generator.predict([z_input, labels_input])
+    # create class labels
+    y = zeros((n_samples, 1))
+    return [images, labels_input], y
+
+# create a line plot of loss for the gan and save to file
+def plot_history(d1_hist, d2_hist, g_hist, a1_hist, a2_hist):
+    # plot loss
+    pyplot.subplot(2, 1, 1)
+    pyplot.plot(d1_hist, label='d-real')
+    pyplot.plot(d2_hist, label='d-fake')
+    pyplot.plot(g_hist, label='gen')
+    pyplot.legend()
+    # plot discriminator accuracy
+    pyplot.subplot(2, 1, 2)
+    pyplot.plot(a1_hist, label='acc-real')
+    pyplot.plot(a2_hist, label='acc-fake')
+    pyplot.legend()
+    # save plot to file
+    pyplot.savefig('plot_line_plot_loss.png')
+    pyplot.close()
+
+# train the generator and discriminator
+def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=300, n_batch=128):
+    bat_per_epo = int(dataset[0].shape[0] / n_batch)
+    half_batch = int(n_batch / 2)
+    # prepare lists for storing stats each iteration
+    d1_hist, d2_hist, g_hist, a1_hist, a2_hist = list(), list(), list(), list(), list()
+    # manually enumerate epochs
+    for i in range(n_epochs):
+        # enumerate batches over the training set
+        for j in range(bat_per_epo):
+            # get randomly selected 'real' samples
+            [X_real, labels_real], y_real = generate_real_samples(dataset, half_batch)
+            # update discriminator model weights
+            d_loss1, d_acc1 = d_model.train_on_batch([X_real, labels_real], y_real)
+            # generate 'fake' examples
+            [X_fake, labels], y_fake = generate_fake_samples(g_model, latent_dim, half_batch)
+            # update discriminator model weights
+            d_loss2, d_acc2 = d_model.train_on_batch([X_fake, labels], y_fake)
+            # prepare points in latent space as input for the generator
+            [z_input, labels_input] = generate_latent_points(latent_dim, n_batch)
+            # create inverted labels for the fake samples
+            y_gan = ones((n_batch, 1))
+            # update the generator via the discriminator's error
+            g_loss = gan_model.train_on_batch([z_input, labels_input], y_gan)
+            # summarize loss on this batch
+            print('>%d, %d/%d, d1=%.3f, d2=%.3f g=%.3f' %
+                (i+1, j+1, bat_per_epo, d_loss1, d_loss2, g_loss))
+            # record history
+            d1_hist.append(d_loss1)
+            d2_hist.append(d_loss2)
+            g_hist.append(g_loss)
+            a1_hist.append(d_acc1)
+            a2_hist.append(d_acc2)
+    plot_history(d1_hist, d2_hist, g_hist, a1_hist, a2_hist)
+    # save the generator model
+    g_model.save('cgan_generator.h5')
+
+
+# size of the latent space
+latent_dim = 100
+# create the discriminator
+d_model = define_discriminator()
+d_model.summary()
+# create the generator
+g_model = define_generator(latent_dim)
+g_model.summary()
+# create the gan
+gan_model = define_gan(g_model, d_model)
+# load image data
+dataset = load_real_samples()
+# train model
+train(g_model, d_model, gan_model, dataset, latent_dim)
